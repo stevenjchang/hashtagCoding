4cae8f9a7b4221bdcddf2f200c5bf59876d9493f
Steve <stevenjchang@gmail.com>
2017-07-23 10:51:44 -0700

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 module.exports = {
-    entry: './main.js',
-    output: {
-        filename: './bundle.js'
-    }
+  entry: './server/index.js',
+  output: {
+      filename: './bundle.js'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  target: "node"
 };
+
