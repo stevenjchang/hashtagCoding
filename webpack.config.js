de5702f1a6d9bb65c4e645d6ffd72593e4f9afb6
Steve <stevenjchang@gmail.com>
2017-07-23 23:42:55 -0700

    basic react/babel/webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
+const path = require('path');
+
 module.exports = {
-  entry: './server/index.js',
+  entry: './client/src/index',
   output: {
-      filename: './bundle.js'
+    path: path.join(__dirname, 'public/dist'),
+    filename: 'bundle.js'
   },
   watch: true,
+  node: {
+    __dirname: true,
+  },
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['es2015', 'react']
         }
       }
     ]
   },
-  target: "node"
+  // target: "node"
 };
-
