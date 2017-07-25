1d4ef98e44dc43f89e5cc5d19c71a938787ebb81
Steve <stevenjchang@gmail.com>
2017-07-24 22:38:17 -0700

    Implement .get(/images) to send back dummyData, removing the api call to instagram

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const path = require('path') 
+const path = require('path')
+var fs = require('fs')
 // const router = require('./router/router.js')
 const router = require('express').Router()
 const IP = process.env.IP || 'localhost'
@@ -8,6 +9,7 @@ const app = express()
 
 const Instagram = require('node-instagram').default
 const ENV = require('../apikey.json')
+const dummyData = require('../database/dummyData').dummyData
  
 // Create a new instance. 
 const instagram = new Instagram({
@@ -19,13 +21,23 @@ const instagram = new Instagram({
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/images', (request, response) => {  
-  instagram.get('users/self/media/recent', (err, data) => {
-    if (err) {
-      console.log('*** Error in instagram.get - app.get(callback) - server/index.js', err);
-    } else {
-      response.send(data);
-    }
-  });
+  // instagram.get('users/self/media/recent', (err, data) => {
+  //   if (err) {
+  //     console.log('*** Error in instagram.get - app.get(callback) - server/index.js', err);
+  //   } else {
+  //     // response.send(data);
+  //     // fs.writeFile(path.join(__dirname, '../database/dummyData2.js'), JSON.stringify(data, null, 2), function(err, success) {
+  //     //   if(err) {
+  //     //     console.log('err inside fs writefile', err)
+  //     //   } else {
+  //     //     console.log('success')
+  //     //   }
+  //     // })
+  //   }
+  // });
+
+  response.send(dummyData);
+
 })
 
 app.listen(PORT, (err) => {  
