03d9cb2a92a015a2ede66d8fc06f4a8b505d8597
Steve <stevenjchang@gmail.com>
2017-07-24 20:27:51 -0700

    Implement basic instagram api call to recent media from user hashtagcoding

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
 const express = require('express')  
 // const router = require('./router/router.js')
 const router = require('express').Router()
-
 const IP = process.env.IP || 'localhost'
 const PORT = process.env.PORT || '3000'
-
 const app = express()  
 
-app.get('/', (request, response) => {  
-  response.send('Hello from Express!')
+const Instagram = require('node-instagram').default
+const ENV = require('../apikey.json')
+ 
+// Create a new instance. 
+const instagram = new Instagram({
+  clientId: ENV.CLIENT_ID,
+  clientSecret: ENV.CLIENT_SECRET ,
+  accessToken: ENV.ACCESS_TOKEN,
+});
+
+app.get('/callback', (request, response) => {  
+  instagram.get('users/self/media/recent', (err, data) => {
+    if (err) {
+      console.log('*** Error in instagram.get - app.get(callback) - server/index.js', err);
+    } else {
+      response.send(data);
+    }
+  });
 })
 
 app.listen(PORT, (err) => {  
