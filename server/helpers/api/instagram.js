c466b37799f88531750eb4d524251dd425d78ca4
stevenjchang <stevenjchang@gmail.com>
2017-07-26 13:10:39 -0700

    refactor instagram call - modularize - create helper/api/instagram

---
+++ server/helpers/api/instagram.js
@@ -0,0 +1,28 @@
+const Instagram = require('node-instagram').default
+const ENV = require('../../../apikey.json')
+
+// Create a new instance. 
+const instagram = new Instagram({
+  clientId: ENV.CLIENT_ID,
+  clientSecret: ENV.CLIENT_SECRET ,
+  accessToken: ENV.ACCESS_TOKEN,
+});
+
+const getInstagramUserImages = (req, res) => {
+  instagram.get('users/self/media/recent', (err, data) => {
+    if (err) {
+      console.log('*** Error in instagram.get - app.get(callback) - server/index.js', err);
+    } else {
+      res.send(data);
+      fs.writeFile(path.join(__dirname, '../database/dummyData2.js'), JSON.stringify(data, null, 2), function(err, success) {
+        if(err) {
+          console.log('err inside fs writefile', err)
+        } else {
+          console.log('success')
+        }
+      })
+    }
+  });
+}
+
+module.export = { getInstagramUserImages: getInstagramUserImages}
