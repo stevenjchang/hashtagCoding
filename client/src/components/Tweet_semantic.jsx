0ef49c8e28fdc41f16bdb52e36daa6e3bfe59be1
stevenjchang <stevenjchang@gmail.com>
2017-07-28 22:42:28 -0700

    Implement randomNumber functions to generate random number of likes and comments

--- client/src/components/Tweet_semantic.jsx
+++ client/src/components/Tweet_semantic.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 
+var randomNumberTo15 = () => Math.floor(Math.random() * 15)
+var randomNumberTo50 = () => Math.floor(Math.random() * 50)
+
 const Tweet_semantic = ({image}) => (
   <div className="ui card">
-    {/* <div className="content">
+     {/* <div className="content">
       <div className="right floated meta">14h</div>
        <img className="ui avatar image" src="https://semantic-ui.com/images/avatar/large/elliot.jpg" /> Elliot
-    </div> */}
+    </div>  */}
     <div className="image">
        <img src={image} />
     </div>
     <div className="content">
       <span className="right floated">
         <i className="heart outline like icon"></i>
-        17 likes
+        {randomNumberTo50()} likes
       </span>
       <i className="comment icon"></i>
-      3 comments
+      {randomNumberTo15()} comments
     </div>
     <div className="extra content">
       <div className="ui large transparent left icon input">
