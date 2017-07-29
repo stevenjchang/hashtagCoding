212d8e57b339283eebba3b2b18432992ae34f154
stevenjchang <stevenjchang@gmail.com>
2017-07-28 18:12:45 -0700

    Init semanitc-ui-less, configer webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,15 @@ module.exports = {
         query: {
           presets: ['es2015', 'react', 'stage-1']
         }
-      }
+      },
+      {
+        test: /\.less/,
+        loader: 'style!css!less',
+      },
+      {
+        test: /\.(png|jpg|gif|woff|svg|eot|ttf|woff2)$/,
+        loader: 'url-loader?limit=1024&name=[name]-[hash:8].[ext]!image-webpack',
+      },
     ]
   },
   // target: "node"
