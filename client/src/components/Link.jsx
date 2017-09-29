3d525f487eb1c64e10cbaca56f80f9ee7ea09735
stevenjchang <stevenjchang@gmail.com>
2017-09-29 15:40:38 -0700

    clean up Link component

--- client/src/components/Link.jsx
+++ client/src/components/Link.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 
-const Link = ({image, text, item}) => {
+const Link = ({item}) => {
   return (
     <div>
-      <h1>Title</h1>
-      <img src={image} style={{height:300, width:300, display: "inline-block"}} />
-      <p>{text}</p>
       <p>item: {item.name}</p>
     </div>
   )
