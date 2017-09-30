18a4b4cb346fbff2a1e28e935aa4b9c0d77cc57d
stevenjchang <stevenjchang@gmail.com>
2017-09-29 19:12:20 -0700

    init contentful basic api call working

--- client/src/components/Links.jsx
+++ client/src/components/Links.jsx
@@ -1,52 +1,67 @@
 import React from 'react'
 import axios from 'axios'
 import Link from './Link.jsx'
+import {createClient} from 'contentful'
+import ENV from '../../../apikey.json'
 
-class Links extends React.Component {
+// class Links extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       links: []
+//     };
+
+//     this.getLinks = this.getLinks.bind(this);
+//   }
+
+//   getLinks() {
+//     axios.get('/links')
+//       .then((result) => this.setState({links: result.data}) )
+//   }
+
+//   render() {
+//     return (
+//       <div>Links
+//         <button onClick={this.getLinks}>Get Links</button>
+//         {this.state.links.map((item, i) => 
+//           <Link item={item} key={i}></Link>
+//         )}
+//       </div>
+//     )
+//   }
+// }
+
+class Links_with_Contentful extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       links: []
-    };
-
+    }
     this.getLinks = this.getLinks.bind(this);
   }
 
   getLinks() {
-    axios.get('/links')
-      .then((result) => this.setState({links: result.data}) )
+    const client = createClient({
+      space: ENV.CONTENTFUL_SPACE_ID,
+      accessToken: ENV.CONTENTFUL_DELIVERY_API
+    })
+    client.getEntries({content_type: 'links'})
+    .then((entry) => {
+      console.log('this is entry =>', entry.items);
+      this.setState({links: entry.items})
+    })
   }
 
   render() {
     return (
       <div>Links
-        <button onClick={this.getLinks}>Get Links</button>
+      <button onClick={this.getLinks}>Get Links</button>
         {this.state.links.map((item, i) => 
-          <Link item={item} key={i}></Link>
-          
-          
+          <Link item={item.fields} key={i}></Link>
         )}
       </div>
     )
   }
 }
 
-export default Links;
-
-// class TweetList extends React.Component {
-//   constructor(props) {
-//     super(props)
-//   }
-  
-//   render() {
-//     const tweetArray = ['hashtag', 'coding', 'willbesuccessful'];
-    
-//     return (
-//       <div>
-//         {this.props.images.map((item, i) => 
-//           <Tweet image={item.image} key={i} text={item.text} /> 
-//         )}   
-//       </div>
-//     )
-//   }
-// }
+export default Links_with_Contentful;
