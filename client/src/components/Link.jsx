4ec0eed1ade347e090312e347bf058d3cd8a0c59
stevenjchang <stevenjchang@gmail.com>
2017-09-30 16:57:47 -0700

    add posted date to render

--- client/src/components/Link.jsx
+++ client/src/components/Link.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
 const Link = ({item}) => {
-  console.log('item =>', item);
-  console.log('item.something =>', item.image.fields.file.url)
+  const dt = item.dateTime;
+  const date = dt[5] + dt[6] + '/' + dt[8] + dt[9] + '/' + dt[0] + dt[1] + dt[2] + dt[3];
   return (
     <div>
       <h2> {item.title} </h2>
-      <a href={item.url} >
-        <img src={item.image.fields.file.url} height="350" width="500"></img>
-      </a>
+      <a href={item.url}> <img src={item.image.fields.file.url} height="350" width="500"></img> </a>
+      <p> posted: {date} </p>
     </div>
   )
 }
