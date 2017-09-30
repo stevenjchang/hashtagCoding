27d6541e5326bc65fc73fa45eafd90c62a6783d9
stevenjchang <stevenjchang@gmail.com>
2017-09-30 16:01:15 -0700

    implement details in Link component

--- client/src/components/Link.jsx
+++ client/src/components/Link.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Link = ({item}) => {
+  console.log('item =>', item);
+  console.log('item.something =>', item.image.fields.file.url)
   return (
     <div>
-      <p>item: {item.name}</p>
+      <h2> {item.title} </h2>
+      <a href={item.url} >
+        <img src={item.image.fields.file.url} height="350" width="500"></img>
+      </a>
     </div>
   )
 }
