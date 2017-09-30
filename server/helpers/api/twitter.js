39c231202c9de17c55281e6d77fb5a4e5dfa69b1
stevenjchang <stevenjchang@gmail.com>
2017-09-30 16:56:14 -0700

    change twitter api call to fetch statuses/user_timeline

--- server/helpers/api/twitter.js
+++ server/helpers/api/twitter.js
@@ -9,9 +9,21 @@ var twitter = new Twitter({
   access_token_secret: ENV.TWITTER_ACCESS_SECRET
 });
 
+// const getTwitterFeed = (req, res) => {
+//   var params = {q: '#iamanengineer OR #hackathon', count: 200, include_entities: true, lang: "en"};
+//   twitter.get('search/tweets', params, function(error, tweets, response) {
+//     if (error) {
+//       console.log('***Error! in getTwitterFeed in server/helpers/twitter =>');
+//     } else {
+//       fs.writeDummyData(tweets, 'dummyData4.js', false)
+//       res.send(tweets);
+//     }
+//   });
+// }
+
 const getTwitterFeed = (req, res) => {
-  var params = {q: '#iamanengineer OR #hackathon', count: 200, include_entities: true, lang: "en"};
-  twitter.get('search/tweets', params, function(error, tweets, response) {
+  var params = {screen_name: 'TechCrunch', count: 200, include_entities: true, lang: "en"};
+  twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (error) {
       console.log('***Error! in getTwitterFeed in server/helpers/twitter =>');
     } else {
