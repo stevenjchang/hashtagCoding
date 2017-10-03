4524f87bdfe5a97389914994ee796661db909749
stevenjchang <stevenjchang@gmail.com>
2017-10-02 21:58:50 -0700

    set carList using cheerio selectors

--- server/helpers/api/craigslist_scraper.js
+++ server/helpers/api/craigslist_scraper.js
@@ -1,26 +1,31 @@
-// const cheerio = require('cheerio')
 const fs = require('fs')
 const path = require('path')
 const cheerio = require('cheerio');
-// const $ = cheerio.load('<h2 class="title">Hello world</h2>')
-
-// const $ = cheerio.load('<ul id="fruits">...</ul>');
 
 const getCraigslistScrapper = () => {
   fs.readFile(path.join(__dirname, '../../../database/craigslist.html'), 'utf-8', function(err, data) {
     if (err) {
       console.log('*** Error! in craigslist_scraper.js');
     } else {
-      const $ = cheerio.load('<h2 class="title">Hello world</h2>')
-        // let $ = cheerio.load(data, { decodeEntities: true });
-      // let $ = cheerio.load('<ul id="fruits">...</ul>');
-      // $('.result-row').each(function(index, element){
-      //   console.log('hey cheerio')
-      // })
-      
-
-      console.log('*** getCraigslistScrapper inside readFile => abc: ')
-    }
+        const $ = cheerio.load(data);
+        const carList = [];
+      $('.result-row').each(function(index, element){
+        carList[index] = {};
+        let resultImage = $(element).find('.result-image');
+        carList[index]['pid'] = $(this).attr('data-pid');
+        carList[index]['href'] = $(resultImage).attr('href')
+        carList[index]['images'] = $(resultImage).attr('data-ids');
+        let price = $(element).find('.result-meta .result-price');
+        carList[index]['price'] = $(price).text();
+        let dateTime = $(element).find('.result-date');
+        carList[index]['dateTime'] = $(dateTime).attr('datetime');
+        let title = $(element).find('.result-title');
+        carList[index]['title'] = $(title).text()
+        let neighborhood = $(element).find('.result-hood');
+        carList[index]['neighborhood'] = $(neighborhood).text()
+      })
+      console.log('*** carList =>', carList)
+      }
   })
 }
 
