c72a7251f54dc76540f062333419cebd4c748c4c
stevenjchang <stevenjchang@gmail.com>
2017-10-03 19:17:11 -0700

    change tab spacing

--- client/src/components/CraigslistListItem.jsx
+++ client/src/components/CraigslistListItem.jsx
@@ -7,15 +7,15 @@ const CraigslistListItem = ({item}) => {
   }
   return (
     <Card.Group>
-    <Card fluid={true}>
-      <Card.Content>
-      <h3>{item.title}</h3>
-      <p>{item.neighborhood} ~ <span style={{color: "red"}}>{item.price}</span></p>
-      {(item.images) ? <img src={'https://images.craigslist.org/' + item.images.split(',')[0].substring(2) + '_300x300.jpg'}></img> : <p>No image</p>}
-      <a href={item.href}><p>link</p></a>
-      <br></br>
-      </Card.Content>
-    </Card>
+      <Card fluid={true}>
+        <Card.Content>
+          <h3>{item.title}</h3>
+          <p>{item.neighborhood} ~ <span style={{color: "red"}}>{item.price}</span></p>
+          {(item.images) ? <img src={'https://images.craigslist.org/' + item.images.split(',')[0].substring(2) + '_300x300.jpg'}></img> : <p>No image</p>}
+          <a href={item.href}><p>link</p></a>
+          <br></br>
+        </Card.Content>
+      </Card>
     </Card.Group>
   )
 }
