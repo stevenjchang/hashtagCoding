50e644d4d33c653ae971a7eec944a1cfbaa6b5a5
stevenjchang <stevenjchang@gmail.com>
2017-10-14 23:39:29 -0700

    refactor to redux - initial changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,78 +1,30 @@
 import React from 'react'
-import axios from 'axios'
-import { Button } from 'semantic-ui-react'
-import MenuBar from './MenuBar.jsx'
-import TweetList from './TweetList.jsx'
-import TweetList_semantic from './TweetList_semantic.jsx'
-import SubmitLink from './SubmitLink.jsx'
+import Footer from './Footer.js'
+import AddTodo from '../containers/AddTodo.js'
+import VisibleTodoList from '../containers/VisibleTodoList.js'
+
+// import CraigslistList from './CraigslistList.jsx'
+import CraigslistList from './redux_components/CraigslistList'
+
 import Links from './Links.jsx'
-import CraigslistList from './CraigslistList.jsx'
+import Menubar from './MenuBar.jsx'
+import SubmitLink from './SubmitLink.jsx'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      imagesFromServer: []
-    }
-    this.getImagesInstagram = this.getImagesInstagram.bind(this)
-    this.getImagesTwitter = this.getImagesTwitter.bind(this)
-  }
+import Craigslist from './../containers/Craigslist.jsx'
+import Twitter from './../containers/Twitter'
 
-  componentDidMount() {
-    // this.getImagesInstagram()
-    // this.getImagesTwitter()
-  }
-
-  getImagesInstagram() {
-    axios.get('/images/instagram')
-      .then((result) => {
-        let cleanResult = result.data.data,
-            imagesArray =[];
-        cleanResult.map((item) => imagesArray.push({image: item.images.low_resolution.url}))
-        this.setState({imagesFromServer: imagesArray})
-      })
-      .catch((err) => console.log('***Error in getImagesFromServer - App.jsx', err))
-  }
-
-  getImagesTwitter() {
-    console.log('twitter call init')
-    axios.get('/images/twitter')
-      .then((result) => {
-        console.log('twitter call received', result)
-        let cleanResult = result.data,
-            imagesArray = [],
-            tweetsArray = [];
-        cleanResult.map((item) => {
-          if (item.entities.media) { 
-            imagesArray.push({
-              image: item.entities.media[0].media_url,
-              text: item.text
-            }) 
-          } else {
-          tweetsArray.push(item.text)
-          }
-        })
-        this.setState({imagesFromServer: imagesArray})
-      })
-      // .then(() => this.setState({imagesFromServer: imagesArray}))
-      .catch((err) => console.log('uh oh error', err))
-  }
-
-  render () {
-    return (
-      <div>
-        <MenuBar />
-        <Button inverted color='blue' onClick={this.getImagesInstagram}>Instagram</Button>
-        <Button inverted color='orange' onClick={this.getImagesTwitter}>Twitter</Button>
-        <Links></Links>
-        <CraigslistList></CraigslistList>
-        {/* <TweetList images={this.state.imagesFromServer} />  */}
-        <TweetList_semantic images={this.state.imagesFromServer} /> 
-        <h1>Complete Form below to submit a link</h1>
-        <SubmitLink></SubmitLink>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <Menubar />
+    <Twitter />
+    <Craigslist />
+    <Links />
+    <Craigslist /> 
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+    <SubmitLink />
+  </div>
+)
 
 export default App
