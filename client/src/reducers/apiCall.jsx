560802154d1b2855ffad85f4574cd48f619969a2
stevenjchang <stevenjchang@gmail.com>
2017-10-14 23:55:25 -0700

    implement reducer apiCall to GET_CRAIGSLIST_FEED

---
+++ client/src/reducers/apiCall.jsx
@@ -0,0 +1,12 @@
+const apiCall = (state = [], action) => {
+  switch (action.type) {
+    case 'GET_CRAIGSLIST_FEED': 
+      return [
+        action.data.data
+      ]
+    default: 
+      return state
+  }
+}
+
+export default apiCall
