079e202e0963fe621d3c549bde4f955efc50f63e
stevenjchang <stevenjchang@gmail.com>
2017-10-14 23:49:35 -0700

    implement actionCreators - getCraigslistFeed

---
+++ client/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const getCraigslistFeed = (dispatch) => {
+  return dispatch => axios.get('/craigslist_scraper')
+    .then(res => {
+      console.log('res2 =>', res) 
+      return res
+    })
+    .then(
+      data => dispatch({ type: 'GET_CRAIGSLIST_FEED', data}),
+      err => dispatch({ type: 'GET_CRAIGSLIST_FEED_ERROR', err })
+    )
+}
