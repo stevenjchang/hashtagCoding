95352d57c497ef29f9060f152368782536da7d21
stevenjchang <stevenjchang@gmail.com>
2017-10-14 23:45:48 -0700

    implement index to render Provider  with store

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,27 @@
+// import React from 'react'
+// import {render} from 'react-dom'
+// import App from './components/App.jsx'
+
+// render(<App/>, document.getElementById('app'))
+
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import todoApp from './reducers'
 import App from './components/App.jsx'
 
-render(<App/>, document.getElementById('app'))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(
+  todoApp,
+  composeEnhancers(applyMiddleware(thunk))
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
+
