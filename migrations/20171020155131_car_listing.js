dde5b8182b55689babd62449d9982b1131161ff2
stevenjchang <stevenjchang@gmail.com>
2017-10-20 16:03:46 -0700

    (db) make migration for car_listing table

---
+++ migrations/20171020155131_car_listing.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+
+    knex.schema.createName('car_listing', function(table) {
+      table.increments('pid').primary();
+      table.string('title');
+      table.string('href');
+      table.string('images');
+      table.string('data-ids');
+      table.string('price');
+      table.string('neighborhood');
+      table.boolean('show');
+      table.dateTime('dateTime');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  
+};
