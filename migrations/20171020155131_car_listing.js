148e8b9fea859c91226b09cc9bbc165e7ea7e9c3
stevenjchang <stevenjchang@gmail.com>
2017-10-21 20:09:39 -0700

    (feat) data array insert to postgres - no duplicate allowed ('pid' is unique)

--- migrations/20171020155131_car_listing.js
+++ migrations/20171020155131_car_listing.js
@@ -3,17 +3,18 @@ exports.up = function(knex, Promise) {
   return Promise.all([
 
     knex.schema.createTable('car_listing', function(table) {
-      table.increments('pid').primary();
+      table.increments('id').primary();
+      table.string('pid').unique();
       table.string('title');
       table.string('href');
-      table.string('images');
+      table.string('images', 500);
       table.string('data-ids');
       table.string('price');
       table.string('neighborhood');
       table.boolean('show');
       table.dateTime('dateTime');
-
     })
+
   ])
 };
 
