3eb662e8efa1b04a4906efc7f4215e0fc3278279
stevenjchang <stevenjchang@gmail.com>
2017-10-25 21:48:55 -0700

    (deploy fix) change env to production

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 var config      = require('../knexfile.js');
-var env         = 'development';  
+// var env         = 'development';  
+var env         = 'production'; 
 var knex        = require('knex')(config[env]);
 
 module.exports = knex;
