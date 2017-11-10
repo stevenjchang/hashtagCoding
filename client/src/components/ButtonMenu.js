642ad076104fef0e7cbd7dee1afc2cc5c1f0737a
stevenjchang <stevenjchang@gmail.com>
2017-11-09 22:51:55 -0800

    (refactor) ButtonMenu changed to stateful component, componentDidMount calls getCraigslistJobs()

--- client/src/components/ButtonMenu.js
+++ client/src/components/ButtonMenu.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 
-const ButtonMenu = ({ onClickContentful, onClickCraigslist, onClick_getCraigslistJobs, onClickInstagram, onClickTwitter }) => (
-  <div>
-    <Button inverted color='purple' onClick={onClickCraigslist}>CraigsList</Button>
-    <Button inverted color='red' onClick={onClickInstagram}>Instagram</Button>
-    <Button inverted color='blue' onClick={onClickTwitter}>Twitter</Button>
-    <Button inverted color='orange' onClick={onClickContentful}>CMS</Button>
-    <Button inverted color='purple' onClick={onClick_getCraigslistJobs}>CL Computer Gigs</Button>
-    <Button inverted color='purple' onClick={onClick_getCraigslistJobs}>CL Event Gigs</Button>
-  </div>
-)
+class ButtonMenu extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    this.props.onClick_getCraigslistJobs();
+  }
+  render() {
+    return (
+      <div>
+        <Button inverted color='purple' onClick={this.props.onClickCraigslist}>CraigsList</Button>
+        <Button inverted color='red' onClick={this.props.onClickInstagram}>Instagram</Button>
+        <Button inverted color='blue' onClick={this.props.onClickTwitter}>Twitter</Button>
+        <Button inverted color='orange' onClick={this.props.onClickContentful}>CMS</Button>
+        <Button inverted color='purple' onClick={this.props.onClick_getCraigslistJobs}>CL Computer Gigs</Button>
+        <Button inverted color='purple' onClick={this.props.onClick_getCraigslistJobs}>CL Event Gigs</Button>
+      </div>
+    )
+  }
+}
 
 export default ButtonMenu
