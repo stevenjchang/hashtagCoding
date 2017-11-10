f3888de8d44143a747a458e662cc13173eed27f2
stevenjchang <stevenjchang@gmail.com>
2017-11-10 00:52:53 -0800

    (css) add bootstrap panel, remove semantic-ui

--- client/src/components/CraigslistJobs_ListItem.js
+++ client/src/components/CraigslistJobs_ListItem.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
 
 const CraigslistJobs_ListItem = ({ item, toggleShowHide }) => {
   return (
-    <Card.Group>
-      <Card fluid={true}>
-        <Card.Content>
-          <h3>{item.title}</h3>
-          <p>{item.neighborhood} ~ <span style={{color: "red"}}>{item.price}</span></p>
-          {(item.images) ? <Image src={'https://images.craigslist.org/' + item.images.split(',')[0].substring(2) + '_300x300.jpg'}></Image> : <p>No image</p>}
-          <a href={item.href}><p>link</p></a>
-          <p>id: {item.id} </p>
-          <p>showStatus: {item.show.toString()}</p>
-          <button onClick={() => toggleShowHide(item.id, item.show)}>Hide Post</button>
-          <br></br>
-        </Card.Content>
-      </Card>
-    </Card.Group>
+    <div className="panel panel-primary">
+      <div className="panel-heading">
+        <h3 className="panel-title">{item.title}</h3>
+        </div>
+        <div className="panel-body">
+        <p>{item.neighborhood} ~ <span style={{color: "red"}}>{item.price}</span></p>
+        {(item.images) ? <img src={'https://images.craigslist.org/' + item.images.split(',')[0].substring(2) + '_300x300.jpg'}></img> : <p>No image</p>}
+        <a href={item.href}><p>link</p></a>
+        <p>id: {item.id} </p>
+        <p>showStatus: {item.show.toString()}</p>
+        <button onClick={() => toggleShowHide(item.id, item.show)}>Hide Post</button>
+        <br></br>
+      </div>
+    </div>
   )
 }
 
