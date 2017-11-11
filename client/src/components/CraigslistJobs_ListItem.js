fc337baec5be2a9e19f442d2ee918eef10484bc8
stevenjchang <stevenjchang@gmail.com>
2017-11-11 13:48:08 -0800

    (css) add bootstrap col and btn

--- client/src/components/CraigslistJobs_ListItem.js
+++ client/src/components/CraigslistJobs_ListItem.js
@@ -2,19 +2,23 @@ import React from 'react'
 
 const CraigslistJobs_ListItem = ({ item, toggleShowHide }) => {
   return (
+    <div className="col-md-8 col-md-offset-2">
     <div className="panel panel-primary">
       <div className="panel-heading">
-        <h3 className="panel-title">{item.title}</h3>
-        </div>
-        <div className="panel-body">
-        <p>{item.neighborhood} ~ <span style={{color: "red"}}>{item.price}</span></p>
-        {(item.images) ? <img src={'https://images.craigslist.org/' + item.images.split(',')[0].substring(2) + '_300x300.jpg'}></img> : <p>No image</p>}
-        <a href={item.href}><p>link</p></a>
-        <p>id: {item.id} </p>
-        <p>showStatus: {item.show.toString()}</p>
-        <button onClick={() => toggleShowHide(item.id, item.show)}>Hide Post</button>
-        <br></br>
+        <a href={item.href} style={{"color": "white"}}>
+        <h3 className="panel-title">
+          {/* <span className="glyphicon glyphicon-play"></span> */}
+            {item.title} 
+          <span style={{"color": "black", "font-style": "italic", "font-size": ".85em"}}> ~ ( {item.neighborhood} )</span></h3></a>
       </div>
+      <div className="panel-body block">
+        {(item.images) ? <img src={'https://images.craigslist.org/' + item.images.split(',')[0].substring(2) + '_300x300.jpg'}></img> : <p>No image</p>}
+        <span className="pull-right">
+        <button className="btn btn-danger btn-xs" onClick={() => toggleShowHide(item.id, item.show)}>Hide Post</button>
+        </span>
+        <button className="btn btn-sucess btn-xs">Show </button>
+      </div>
+    </div>
     </div>
   )
 }
