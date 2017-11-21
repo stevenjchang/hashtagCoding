e9394f000907c3faa8ebfc522274a3d9080b52da
stevenjchang <stevenjchang@gmail.com>
2017-11-20 17:11:01 -0800

    (css) remove semantic-ui in Menu

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -5,28 +5,26 @@ class MenuBar extends React.Component {
   constructor(props) {
     super(props) 
     this.state = { activeItem: 'home' }
-
   }
 
   handleItemClick(e, { name }) {
     this.setState({ activeItem: name })
-    console.log('state =>', this.state)
   }
 
   render() {
     const { activeItem } = this.state
-    console.log('222=>',this.state)
-    return (
-      <Segment inverted color='blue'>
-        <Menu inverted secondary color='blue' inverted widths={7}>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
-          <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
-          <Menu.Item name='projects' active={activeItem === 'projects'} onClick={this.handleItemClick} />
-        </Menu>
-      </Segment>
-    )
+    // return (
+    //   <Segment inverted color='blue'>
+    //     <Menu inverted secondary color='blue' inverted widths={7}>
+    //       <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+    //       <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+    //       <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+    //       <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
+    //       <Menu.Item name='projects' active={activeItem === 'projects'} onClick={this.handleItemClick} />
+    //     </Menu>
+    //   </Segment>
+    // )
+    return ( <div></div>)
   }
 }
 
