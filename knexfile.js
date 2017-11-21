99090929bbced6c8d149b407bf9b265b6bafd7d2
stevenjchang <stevenjchang@gmail.com>
2017-11-21 10:54:14 -0800

    (fix) set knexfile to connection: process.env.DATABASE_URL + '? ssl=true' only

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
-const db_connection = process.env.DATABASE_URL ? process.env.DATABASE_URL + '? ssl=true' : { database: 'blog_dev' }
+// const db_connection = process.env.DATABASE_URL ? process.env.DATABASE_URL + '? ssl=true' : { database: 'blog_dev' }
 
 module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: db_connection
+    connection: process.env.DATABASE_URL + '? ssl=true'
   }
 
 }
