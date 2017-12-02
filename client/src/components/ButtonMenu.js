8f457b9008fa9d47a952d65f1aca9c25958fa015
stevenjchang <stevenjchang@gmail.com>
2017-12-02 01:09:49 -0800

    (css) add h2/h4 title to site

--- client/src/components/ButtonMenu.js
+++ client/src/components/ButtonMenu.js
@@ -10,6 +10,8 @@ class ButtonMenu extends React.Component {
   render() {
     return (
       <div className='text-center' style={{marginBottom:'35px', marginTop:'25px'}}>
+        <h2>Steve's Job Search Aggregator</h2>
+        <h4>See all your job searches at once</h4>
         <button className='btn btn-default btn-sm' inverted color='purple' onClick={this.props.onClickCraigslist}>CraigsList</button>
         <button className='btn btn-default btn-sm' inverted color='red' onClick={this.props.onClickInstagram}>Instagram</button>
         <button className='btn btn-default btn-sm' inverted color='blue' onClick={this.props.onClickTwitter}>Twitter</button>
