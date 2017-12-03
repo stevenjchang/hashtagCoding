6892db6a665ad4c0f4415b22d2c84a04b73fd333
stevenjchang <stevenjchang@gmail.com>
2017-12-02 18:59:57 -0800

    (feat) getGlassdoorJobs res.sends sortedData object

--- server/helpers/api/glassdoor.js
+++ server/helpers/api/glassdoor.js
@@ -4,25 +4,29 @@ const db = require('../../db');
 const scraper = require('../scraper/');
 
 const url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=front+end&sc.keyword=front+end&locT=C&locId=1147401&jobType=";
+const urlPrefix = "https://www.glassdoor.com/";
 
 const getGlassdoorJobs = (req, res) => {
   axios.get(url)
     .then(result => {
       const $ = cheerio.load(result.data);
       const jobList = [];
-
       $('.jlGrid').children().each(function(index,element) {
         let jobLink = $(element).find('.jobLink');
-        let href = $(jobLink).attr('href');
         let info = $(element).find('.infoSalEst');
-        let employer = $(info).attr('data-employer-shortname');
-        let jobtitle = $(info).attr('data-jobtitle');
-        let id = $(info).attr('data-job-id');
-        let location = $(element).find('.empLoc').find('.loc').text();
+        jobList[index] = {};
+        jobList[index]['href'] = urlPrefix + $(jobLink).attr('href');
+        jobList[index]['employer'] = $(info).attr('data-employer-shortname');
+        jobList[index]['jobtitle'] = $(info).attr('data-jobtitle');
+        jobList[index]['pid'] = $(info).attr('data-job-id');
+        jobList[index]['location'] = $(element).find('.empLoc').find('.loc').text();
       })
-
+      return jobList;
     })
-    .catch((err) => console.log('Error! in getGlassdoorJobs - api/glassdoor.js =>', err))
+    .then(sortedData => {
+      res.send(sortedData);
+    })
+    .catch(err => console.log('Error! in getGlassdoorJobs - api/glassdoor.js =>', err))
 }
 
 module.exports.getGlassdoorJobs = getGlassdoorJobs;
