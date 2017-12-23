40461a454d76d737106ecc54b8843502b76cdbd2
stevenjchang <stevenjchang@gmail.com>
2017-12-22 22:29:09 -0800

    (fix) renamed 'this' to 'element' to solve pid === undefined issue
    when any undefined value is inserted into the db, it is rejected

--- server/helpers/api/craigslist.js
+++ server/helpers/api/craigslist.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const db = require('../../db');
 const scraper = require('../scraper/');
+
 const { scrapeCraigslist } = scraper.craigslist;
 
 const urlAddress = 'https://sfbay.craigslist.org/search/cto?hasPic=1&min_price=2500&max_price=6000&min_auto_year=2005&auto_title_status=1';
@@ -21,15 +22,15 @@ const getCraigslistFeed = (req, res) => {
       $('.result-row').each((index, element) => {
         carList[index] = {};
         let resultImage = $(element).find('.result-image');
-        carList[index]['pid'] = $(this).attr('data-pid');
-        carList[index]['href'] = $(resultImage).attr('href')
+        carList[index]['pid'] = $(element).attr('data-pid');
+        carList[index]['href'] = $(resultImage).attr('href');
         carList[index]['images'] = $(resultImage).attr('data-ids');
         let price = $(element).find('.result-meta .result-price');
         carList[index]['price'] = $(price).text();
         let dateTime = $(element).find('.result-date');
         carList[index]['dateTime'] = $(dateTime).attr('datetime');
         let title = $(element).find('.result-title');
-        carList[index]['title'] = $(title).text()
+        carList[index]['title'] = $(title).text();
         let neighborhood = $(element).find('.result-hood');
         let neighborhoodCleanText = $(neighborhood).text().substring(2, $(neighborhood).text().length - 1);
         carList[index]['neighborhood'] = neighborhoodCleanText;
@@ -39,7 +40,9 @@ const getCraigslistFeed = (req, res) => {
     })
     .then((sortedData) => {
       sortedData.map((item, i) => {
-        const { pid, title, href, images, price, neighborhood, dateTime, show } = item;
+        let { pid, title, href, images, price, neighborhood, dateTime, show } = item;
+        images = images || '';
+        neighborhood = neighborhood || '';
         db.raw(
           'INSERT INTO car_listing (pid, title, href, images, price, neighborhood, "dateTime", show) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING',
           [pid, title, href, images, price, neighborhood, dateTime, show],
