5303c851dd9461d5c2a4ad378f86bee4d7dd1288
stevenjchang <stevenjchang@gmail.com>
2017-12-23 14:33:15 -0800

    update css with col-4 and image height

--- client/src/components/CraigslistList.js
+++ client/src/components/CraigslistList.js
@@ -4,19 +4,23 @@ import CraigslistListItem from './CraigslistListItem';
 const CraigslistList = ({ list, onButtonClick, toggleShowHide }) => {
   const renderedList = list[0] || [];
   return (
-    <div>
-      {renderedList.map((item, i) => {
-        if (item.show) {
+    <div className="container">
+      <div className="row">
+        {renderedList.map((item, i) => {
+          if (item.show) {
+            return (
+              <div className="col-4">
+                <CraigslistListItem item={item} key={i} toggleShowHide={toggleShowHide} />
+              </div>
+            );
+          }
           return (
-            <CraigslistListItem item={item} key={i} toggleShowHide={toggleShowHide} />
+            <div>
+              <h3>CraigslistList</h3>
+            </div>
           );
-        }
-        return (
-          <div>
-            <h3>CraigslistList</h3>
-          </div>
-        );
-      })}
+        })}
+      </div>
     </div>
   );
 };
--- client/src/components/CraigslistListItem.js
+++ client/src/components/CraigslistListItem.js
@@ -6,19 +6,41 @@ const createImageSourceUrl = (imageId) => {
 
 const CraigslistListItem = ({ item, toggleShowHide }) => {
   return (
-    <div className="container">
-      <h3>{item.title}</h3>
-      <p>{item.neighborhood} ~ <span style={{ color: 'red' }}>{item.price}</span></p>
-      {(item.images) ?
-        <img
-          src={createImageSourceUrl(item.images)}
-        />
-        : <p>No image</p>}
-      <a href={item.href}><p>link</p></a>
-      <p>id: {item.id} </p>
-      <p>showStatus: {item.show.toString()}</p>
-      <button onClick={() => toggleShowHide(item.id, item.show)}>Hide Post</button>
-      <br />
+    <div className="">
+      <div className="panel panel-primary">
+        <a href={item.href} target="_blank">
+          <p className="panel-title listing-title">
+            {item.title}
+            <span
+              className="listing-title-neighborhood"> ~ ( {item.neighborhood ? item.neighborhood : 'no location'}
+              )
+            </span>
+            <span style={{ color: 'red' }}>{item.price}</span>
+          </p>
+        </a>
+      </div>
+      <div className="panel-body block">
+        <div className="panel-body-img-car">
+          {(item.images) ?
+            <img
+              className="img-test"
+              src={createImageSourceUrl(item.images)}
+            />
+          : <img
+            className="panel-body-img"
+            src="http://via.placeholder.com/65x65"
+            alt="job-listing"
+          />}
+        </div>
+        <span className="pull-right">
+          <button className="btn btn-success btn-sm">Save Job</button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => toggleShowHide(item.id, item.show)}
+          >Hide Job
+          </button>
+        </span>
+      </div>
     </div>
   );
 };
