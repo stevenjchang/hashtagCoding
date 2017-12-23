1ef403830b3cd6d092ce3f823ddf6a5c696bdfa1
stevenjchang <stevenjchang@gmail.com>
2017-12-22 23:22:16 -0800

    (fix) the img src url to concat correctly
    add helper function createImageSourceUrl to create the url

--- client/src/components/CraigslistListItem.js
+++ client/src/components/CraigslistListItem.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+
+const createImageSourceUrl = (imageId) => {
+  return `https://images.craigslist.org/${imageId.split(',')[0].substring(2)}_300x300.jpg`;
+};
 
 const CraigslistListItem = ({ item, toggleShowHide }) => {
   return (
-    <Card.Group>
-      <Card fluid={true}>
-        <Card.Content>
-          <h3>{item.title}</h3>
-          <p>{item.neighborhood} ~ <span style={{ color: 'red' }}>{item.price}</span></p>
-          {(item.images) ?
-            <Image
-              src={
-                `https://images.craigslist.org/, 
-                ${item.images.split(',')[0].substring(2)}, 
-                _300x300.jpg`
-              }
-            />
-            : <p>No image</p>}
-          <a href={item.href}><p>link</p></a>
-          <p>id: {item.id} </p>
-          <p>showStatus: {item.show.toString()}</p>
-          <button onClick={() => toggleShowHide(item.id, item.show)}>Hide Post</button>
-          <br />
-        </Card.Content>
-      </Card>
-    </Card.Group>
+    <div>
+      <h3>{item.title}</h3>
+      <p>{item.neighborhood} ~ <span style={{ color: 'red' }}>{item.price}</span></p>
+      {(item.images) ?
+        <img
+          src={createImageSourceUrl(item.images)}
+        />
+        : <p>No image</p>}
+      <a href={item.href}><p>link</p></a>
+      <p>id: {item.id} </p>
+      <p>showStatus: {item.show.toString()}</p>
+      <button onClick={() => toggleShowHide(item.id, item.show)}>Hide Post</button>
+      <br />
+    </div>
   );
 };
 
