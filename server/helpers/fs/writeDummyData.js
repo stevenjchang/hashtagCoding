f1e1cd7b1a3401794f501a0ad6a59b6f35eba032
stevenjchang <stevenjchang@gmail.com>
2018-05-22 18:23:02 -0700

    toggle fs.writeFile config to write js file (module.exports), instead of html

--- server/helpers/fs/writeDummyData.js
+++ server/helpers/fs/writeDummyData.js
@@ -4,10 +4,17 @@ const path = require('path');
 const writeDummyData = (data, filename, boolean) => {
   const prefix = 'module.exports = \n';
   if (boolean) { // set boolean to true to turn function on
+    /**
+     * in fs.writeFile:
+     * - to write .js file that can be imported/exported,
+     *   the second arg should be prefix + JSON.Stringify
+     * - to write .html
+     *   the second arg should just be data
+     */
     fs.writeFile(
       path.join(__dirname, '../../../database/', filename),
-      // prefix + JSON.stringify(data, null, 2),
-      data,
+      prefix + JSON.stringify(data, null, 2),
+      // data,
       (err, success) => {
         if (err) {
           console.log('***Error! in writeDummyData - helpers/fs/writeFile', err);
