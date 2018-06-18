90562dfe4eca9fa3a2a149095bfafddc7bc68647
stevenjchang <stevenjchang@gmail.com>
2018-06-17 21:55:16 -0700

    deploy fix - change proces.env

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const config = require('../knexfile.js');
-// const env = process.env.NODE_ENV || 'development';
-const env = 'development';
+
+const env = process.env.NODE_ENV || 'development';
+// const env = 'development';
 const knex = require('knex')(config[env]);
 
 module.exports = knex;
